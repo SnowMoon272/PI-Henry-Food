a28862827a1ddf174beff317acbe0c4694185b10
Manuel Roberto Serrano Torres <CastielAltair0027@outlook.com>
2022-07-29 22:26:39 -0500

    TY

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -1,32 +1,67 @@
+/* eslint-disable react/no-danger */
 /* eslint-disable react/destructuring-assignment */
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import { getDetails } from "../../redux/actions/index";
 import NavBar from "../Header/NavBar";
+import imgDefaul from "../../img/fondoManu.jpg";
 
 const H1Style = styled.h1`
+  display: inline-block;
+
+  position: relative;
   margin: 0px;
-  padding-top: 100px;
+  padding-top: 80px;
   color: white;
-  font-size: 4rem;
+  font-size: 5rem;
+  top: -84px;
 `;
 
 const MainStyle = styled.main`
   box-sizing: border-box;
-  border: solid blue 3px;
   width: 1400px;
   height: 100vh;
   margin: 0px auto;
+  color: white;
 `;
 
 const SectionStyleCon = styled.section`
   box-sizing: border-box;
-  border: solid #03ff03 3px;
+  /* border: solid #03ff03 3px; */
   width: 100%;
   height: fit-content;
+  border-radius: 25px;
+  background-color: #0000008f;
 
-  margin: 0px auto;
+  & h1 {
+    display: inline-block;
+    font-size: 4rem;
+    border-bottom: 3px solid #fff;
+  }
+  & .Img-Summary {
+    display: flex;
+
+    & img {
+      object-fit: cover;
+      width: 550px;
+      height: 350px;
+      border-end-start-radius: 120px;
+      border-top-right-radius: 80px;
+      border-top-left-radius: 15px;
+      border-end-end-radius: 15px;
+      margin: 40px;
+      box-shadow: 0px 0px 25px 7px #ffffff;
+    }
+
+    & p {
+      font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif, Geneva, Verdana, sans-serif;
+      padding: 20px 20px 20px 55px;
+      margin: 10px;
+      text-align: justify;
+      font-size: 2.2rem;
+    }
+  }
 `;
 
 function Detail(props) {
@@ -43,16 +78,18 @@ function Detail(props) {
       <MainStyle>
         <H1Style>Recipe Detail</H1Style>
         <SectionStyleCon>
-          {theRecipe.title ? (
-            <div>
-              <h1>{theRecipe.title}</h1>
-              <img src={theRecipe.image} alt="gg" />
-              <p>{theRecipe.summary}</p>
-              {/* <h3>{theRecipe.diets}</h3> */}
-            </div>
-          ) : (
-            <div>Not found</div>
-          )}
+          {theRecipe.title && <h1>{theRecipe.title}</h1>}
+          <div className="Img-Summary">
+            {theRecipe.summary && (
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: theRecipe.summary,
+                }}
+              />
+            )}
+            {theRecipe.image && <img src={theRecipe.image || imgDefaul} alt="img not foud" />}
+          </div>
+          <div className="Content">Hola</div>
         </SectionStyleCon>
       </MainStyle>
     </>
@@ -61,27 +98,6 @@ function Detail(props) {
 
 export default Detail;
 
-/*
-<img src={image || imgDefaul} alt="img not foud" />
-<div>
-  <h2>{title}</h2>
-  <h3>
-    {url === "http://localhost:3000/recipe"
-      ? diets.map((diet) => {
-          return ` ${diet.charAt(0).toUpperCase() + diet.slice(1)} |`;
-        })
-      : getDiets()}
-  </h3>
-  <h3>{`Health Score :  ${healthScore}%`}</h3>
-  {url !== "http://localhost:3000/recipe" ? (
-    <LinkStayled to={`/detail/${id}`}>
-      <button id="BTN-CArd" type="button">
-        Learn More
-      </button>
-    </LinkStayled>
-  ) : (
-    <button id="BTN-CArd" type="button">
-      Example
-    </button>
-  )}
-</div> */
+{
+  /* <h3>{theRecipe.diets}</h3> */
+}
