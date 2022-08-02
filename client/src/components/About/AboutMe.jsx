b3d0518896549f2598903321a4a295821a6acf59
Manuel Roberto Serrano Torres <CastielAltair0027@outlook.com>
2022-08-02 00:12:10 -0500

    Aboutme Terminado

--- client/src/components/About/AboutMe.jsx
+++ client/src/components/About/AboutMe.jsx
@@ -4,247 +4,173 @@ import styled from "styled-components";
 import NavBar from "../Header/NavBar";
 // font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif, Geneva, Verdana, sans-serif;
 
+const HeaderStyleCont = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TecnologisStyleCont = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & img {
+    width: 50px;
+    height: 50px;
+    margin: 10px 35px;
+  }
+`;
+
 const HeaderStyle = styled.header`
-  font-family: "Dancing Script", cursive;
-  margin-top: 100px;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif, Geneva, Verdana, sans-serif;
+  box-sizing: border-box;
 
-  padding: 25px;
-  max-width: 800px;
-  width: 1500px;
-  border-radius: 10px;
-  box-shadow: 10px 10px 30px rgba(0, 0, 0, 0.935);
-  background-color: #0000008f;
-
+  margin-top: 150px;
+  font-size: 2rem;
+  padding: 50px;
+  /*  max-width: 800px; */
+  width: 1200px;
+  border-radius: 35px;
+  background-color: #000000af;
   color: #ffffff;
-  user-select: none;
 
-  .href-henry {
+  & h1 {
+    font-family: "Dancing Script", cursive;
+    border-bottom: 3px solid #af911a;
+    color: #af911a;
+    display: inline-block;
+    font-size: 5rem;
+    margin: 0px 0px 25px 0px;
+  }
+
+  & h2 {
+    font-family: "Dancing Script", cursive;
+    border-bottom: 3px solid #af911a;
+    color: #af911a;
+    display: inline-block;
+    font-size: 3rem;
+    margin: 35px 0px 25px 0px;
+  }
+
+  & .Links {
     color: rgb(255, 196, 0);
     text-transform: uppercase;
-  }
 
-  .href-henry:hover {
-    color: rgb(228, 175, 0);
+    & :hover {
+      color: rgb(228, 175, 0);
+    }
   }
-
-  .href-spoon {
-    color: rgb(255, 196, 0);
-    text-transform: uppercase;
-  }
-
-  .href-spoon:hover {
-    color: rgb(228, 175, 0);
-  }
-
-  .tech-title {
-    margin-top: 20px;
+  & .Oculto {
     color: black;
-    font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande", "Lucida Sans", Arial,
-      sans-serif;
-    text-transform: uppercase;
+    background: black;
   }
 
-  .tech-container {
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-    margin-bottom: 20px;
+  & .Span {
+    color: #af911a;
   }
 
-  .javascript {
-    margin: 15px;
-  }
-
-  .img-javascript {
-    width: 45px;
-    height: 45px;
-    margin-left: 16px;
-  }
-
-  .text-javascript {
-    font-size: 15px;
-    text-align: center;
-  }
-
-  .html {
-    margin: 15px;
-  }
-
-  .img-html {
-    width: 45px;
-    height: 45px;
-  }
-
-  .text-html {
-    font-size: 15px;
-    text-align: center;
-  }
-
-  .css {
-    margin: 15px;
-  }
-
-  .img-css {
-    width: 45px;
-    height: 45px;
-  }
-
-  .text-css {
-    font-size: 15px;
-    text-align: center;
-  }
-
-  .react {
-    margin: 15px;
-  }
-
-  .img-react {
-    width: 45px;
-    height: 45px;
-  }
-
-  .text-react {
-    font-size: 15px;
-    text-align: center;
-  }
-
-  .redux {
-    margin: 15px;
-  }
-
-  .img-redux {
-    width: 45px;
-    height: 45px;
-  }
-
-  .text-redux {
-    font-size: 15px;
-    text-align: center;
-  }
-
-  .express {
-    margin: 15px;
-  }
-
-  .img-express {
-    width: 45px;
-    height: 45px;
-    margin-left: 5px;
-  }
-
-  .text-express {
-    font-size: 15px;
-    text-align: center;
-  }
-
-  .PostgreSQL {
-    margin: 15px;
-  }
-
-  .img-PostgreSQL {
-    width: 45px;
-    height: 45px;
-    margin-left: 20px;
-  }
-
-  .text-PostgreSQL {
-    font-size: 15px;
-    text-align: center;
+  & h6 {
+    position: absolute;
+    bottom: 120px;
+    right: 370px;
   }
 `;
 
 export default function About() {
   return (
-    <HeaderStyle className="container">
+    <HeaderStyleCont>
       <NavBar />
-      <div className="about">
+      <HeaderStyle>
         <h1>Henry Food</h1>
         <p>
-          Este proyecto fue creoado como parte de mi formacion como desarrollador Fullstack en
+          Este proyecto fue creado como parte de mi formación como desarrollador Fullstack en
           <a
-            className="href-henry"
+            className="Links"
             href="https://www.soyhenry.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Henry bootcamp
+            " Henry bootcamp "
           </a>
-          . In order to map all the different recipess food, this app consumes data from{" "}
+          . Se utilizó una API externa para poder mostrar toda la gama de recetas que este sitio
+          ofrece:
           <a
-            className="href-spoon"
-            href="https://spoonacular.com/"
+            className="Links"
+            href="https://spoonacular.com/food-api"
             target="_blank"
             rel="noopener noreferrer"
           >
-            the food API
+            " Spoonacular "
           </a>
-          . It is also possible to create a new recipe food, entering a name, score, health score
-          and instructions on how to prepare a delicious dish.
+          . Adicional a esto se le sumó al proyecto una base de datos local, mediante la cual
+          podemos agregar nuestras propias recetas, cuenta con filtros y ordenamientos que ayudan a
+          navegar entre las recetas.
         </p>
         <p>
-          Any feedback you can provide will be greatly appreciated. Thank you, and don't forget to
-          create your recipe food!
+          Aquí comparto el code de Git Hub para analizar su código:{" "}
+          <span className="Oculto">xxxx-xxxx-xxxx-xxxx</span> <br />
+          Gracias por visitar mi sitio. Att: <span className="Span">Manuel S.</span> <br /> Email:{" "}
+          <span className="Span">CastielAltair0027@outlook.com</span>
+          <br />
+          <h6>
+            Alias: <span className="Span Alias">SnowMoon</span>{" "}
+          </h6>
         </p>
-      </div>
-      <h1 className="tech-title">Used technologies:</h1>
-      <div className="tech-container">
-        <div className="javascript">
-          <img
-            className="img-javascript"
-            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
-            alt="javascript"
-          />
-          <h1 className="text-javascript">Javascript</h1>
-        </div>
-        <div className="html">
-          <img
-            className="img-html"
-            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
-            alt="html"
-          />
-          <h1 className="text-html">HTML</h1>
-        </div>
-        <div className="css">
-          <img
-            className="img-css"
-            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
-            alt="css"
-          />
-          <h1 className="text-css">CSS</h1>
-        </div>
-        <div className="react">
-          <img
-            className="img-react"
-            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
-            alt="react"
-          />
-          <h1 className="text-react">React</h1>
-        </div>
-        <div className="redux">
-          <img
-            className="img-redux"
-            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg"
-            alt="redux"
-          />
-          <h1 className="text-redux">Redux</h1>
-        </div>
-        <div className="express">
-          <img
-            className="img-express"
-            src="https://w7.pngwing.com/pngs/212/722/png-transparent-web-development-express-js-javascript-software-framework-laravel-world-wide-web-purple-blue-text.png"
-            alt="express"
-          />
-          <h1 className="text-express">Express</h1>
-        </div>
-        <div className="PostgreSQL">
-          <img
-            className="img-PostgreSQL"
-            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg"
-            alt="PostgreSQL"
-          />
-          <h1 className="text-PostgreSQL">PostgreSQL</h1>
-        </div>
-      </div>
-    </HeaderStyle>
+
+        <h2>Tecnologías utilizadas:</h2>
+        <TecnologisStyleCont>
+          <div>
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+              alt="javascript"
+            />
+            <h3 className="text-javascript">Javascript</h3>
+          </div>
+          <div>
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
+              alt="html"
+            />
+            <h3>HTML</h3>
+          </div>
+          <div>
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
+              alt="css"
+            />
+            <h3>CSS</h3>
+          </div>
+          <div>
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
+              alt="react"
+            />
+            <h3>React</h3>
+          </div>
+          <div>
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg"
+              alt="redux"
+            />
+            <h3>Redux</h3>
+          </div>
+          <div>
+            <img
+              src="https://w7.pngwing.com/pngs/212/722/png-transparent-web-development-express-js-javascript-software-framework-laravel-world-wide-web-purple-blue-text.png"
+              alt="express"
+            />
+            <h3>Express</h3>
+          </div>
+          <div>
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg"
+              alt="PostgreSQL"
+            />
+            <h3>PostgreSQL</h3>
+          </div>
+        </TecnologisStyleCont>
+      </HeaderStyle>
+    </HeaderStyleCont>
   );
 }
